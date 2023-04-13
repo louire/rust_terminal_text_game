959ddc25d29d21eefe7b42e81f58e47f518a1ab5
louire <devlouirecio@gmail.com>
2023-04-13 18:53:56 -0500

    using assets in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use csv::{ReaderBuilder, StringRecord};
+use std::{fs};
+
+const FILENAME: &str = "history.csv";
 fn main() {
-    println!("Hello, world!");
+    let content = fs::read_to_string(FILENAME).unwrap();
+    let mut rdr = ReaderBuilder::new().delimiter(b';').from_reader(content.as_bytes());
+
+    for result in rdr.records() {
+
+        println!("Texto: {}", result.unwrap().get(2).unwrap().trim());
+
+
+
+    }
+
+
 }
